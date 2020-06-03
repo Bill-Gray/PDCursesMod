c79ac9d72e5b1155e5d77aea6e1b125af6735bb3
Gianluigi Tiesi <sherpya@netfarm.it>
2020-06-03 07:20:46 +0200

    cmake: mingw compatibility
    - avoid settings /SUBSYSTEM:WINDOWS directly
    - moved project directive: accidentally fixes also if(MSCV) since
      MINGW, MSVC, CYGWIN etc. are undefined until project is declared

--- cmake/project_common.cmake
+++ cmake/project_common.cmake
@@ -110,7 +110,7 @@ macro (demo_app dir targ)
         set(src_files ${CMAKE_CURRENT_SOURCE_DIR}/${dir}/${targ}.c)
     endif()
 
-    add_executable(${bin_name} ${src_files})
+    add_executable(${bin_name} ${ARGV2} ${src_files})
 
     target_link_libraries(${bin_name} ${PDCURSE_PROJ} ${EXTRA_LIBS})
 
