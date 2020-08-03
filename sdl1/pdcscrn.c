350b0893e86d4c9d492f3a54d35031084e3c5e63
Bill-Gray <pluto@projectpluto.com>
2020-08-03 11:55:35 -0400

    SDL1 lacked PDC_set_function_key and PDC_set_resize_limits.

--- sdl1/pdcscrn.c
+++ sdl1/pdcscrn.c
@@ -362,3 +362,16 @@ int PDC_init_color(int color, int red, int green, int blue)
 
     return OK;
 }
+
+int PDC_set_function_key( const unsigned function, const int new_key)
+{
+   return( 0);
+}
+
+void PDC_set_resize_limits( const int new_min_lines,
+                            const int new_max_lines,
+                            const int new_min_cols,
+                            const int new_max_cols)
+{
+   return;
+}
