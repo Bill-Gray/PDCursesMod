78e6949bbd304e3a41bcb687c398a0820b9100a9
loblolly986 <66034630+loblolly986@users.noreply.github.com>
2020-09-01 17:37:46 -0400

    WinCon: play beeps synchronously so multiple beeps work.  Resolves issue #176.

--- wingui/pdcutil.c
+++ wingui/pdcutil.c
@@ -1,19 +1,25 @@
 /* Public Domain Curses */
 
 #include "pdcwin.h"
+#ifdef WIN32_LEAN_AND_MEAN
+#include <mmsystem.h>
+#endif
+
+extern CRITICAL_SECTION PDC_cs;
 
 void PDC_beep(void)
 {
     PDC_LOG(("PDC_beep() - called\n"));
 
-/*  MessageBeep(MB_OK); */
-    MessageBeep(0XFFFFFFFF);
+    LeaveCriticalSection(&PDC_cs);
+    if (!PlaySound((LPCTSTR) SND_ALIAS_SYSTEMDEFAULT, NULL, SND_ALIAS_ID))
+        Beep(800, 200);
+    EnterCriticalSection(&PDC_cs);
 }
 
 void PDC_napms(int ms)     /* 'ms' = milli,  _not_ microseconds! */
 {
     /* RR: keep GUI window responsive while PDCurses sleeps */
-    extern CRITICAL_SECTION PDC_cs;
 
     PDC_LOG(("PDC_napms() - called: ms=%d\n", ms));
 
