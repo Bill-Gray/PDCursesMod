4996fb06d29d622bb53178a7cd1a4913328ef4d5
Bill-Gray <pluto@projectpluto.com>
2022-02-05 15:26:17 -0500

    'initscr' : ensure that all memory is freed after delscreen()/endwin().  It'll get freed by the system anyway,  but this ensures that Valgrind doesn't issue a sort-of-spurious warning

--- pdcurses/initscr.c
+++ pdcurses/initscr.c
@@ -341,6 +341,8 @@ SCREEN *set_term(SCREEN *new)
     return (new == SP) ? SP : NULL;
 }
 
+void PDC_free_pair_hash_table( void);        /* color.c */
+
 void delscreen(SCREEN *sp)
 {
     PDC_LOG(("delscreen() - called\n"));
@@ -352,6 +354,7 @@ void delscreen(SCREEN *sp)
     free(SP->c_ungch);
     free(SP->c_buffer);
     free(SP->atrtab);
+    PDC_free_pair_hash_table();
 
     PDC_slk_free();     /* free the soft label keys, if needed */
 
