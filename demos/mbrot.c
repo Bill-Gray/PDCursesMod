b8e8a9098b19eecc472e09011d56e51baea9a60a
Bill-Gray <pluto@projectpluto.com>
2022-05-09 10:54:12 -0400

    'mbrot' : slightly better approximate natural log function

--- demos/mbrot.c
+++ demos/mbrot.c
@@ -139,34 +139,40 @@ static void show_mandelbrot( const double x0, const double y0,
     }
 }
 
-/* The following will return a value within 2.8e-5 of the actual
-natural log of x.  Its only advantage is that it avoids a need
-to link in the math library. */
+/* The following will return a value within 1.3e-6 of the actual natural
+log of x.  Its only advantage is that it avoids a need to link in the
+math library.  It uses the first three terms of the series expansion
+
+ln( x) = 2(z + z^3/3 + z^5/5 + z^7/7 + ...)
+
+where z = (x-1) / (x+1).  For sqrt(.5) < x < sqrt(2), -0.1716 < z < 0.1716,
+so convergence is good enough for this particular use case.
+
+n terms     worst error                   n terms     worst error
+ 1          3.5e-3                         4          2.9e-8
+ 2          6.1e-5                         5          7.1e-10
+ 3          1.3e-6   (this code)           6          1.8e-11        */
 
 static double _approx_ln( double x)
 {
-   const double tenth_root_of_two =
-          1.0717734625362931642130063250233420229063846049775567834827806681442454;
-   const double fifth_root_of_two =
-          1.1486983549970350067986269467779275894438508890977975055137111184936032;
+   const double sqrt_2 =
+          1.4142135623730950488016887242096980785696718753769480731766797379907325;
    const double ln_2 =
           0.6931471805599453094172321214581765680755001343602552541206800094933936;
-   int n_fifths = 0;
+   int two_power = 0;
+   double z, z2;
 
-   if( x < tenth_root_of_two / 2.)
+   assert( x > 0.);
+   if( x < sqrt_2 / 2.)
       return( -_approx_ln( 1. / x));
-   while( x > tenth_root_of_two)
+   while( x > sqrt_2)
       {
       x /= 2.;
-      n_fifths += 5;
+      two_power++;
       }
-   while( x < 1. / tenth_root_of_two)
-      {
-      x *= fifth_root_of_two;
-      n_fifths--;
-      }
-   x = (x - 1.) / (x + 1.);
-   return( 2. * x + (double)n_fifths * ln_2 / 5.);
+   z = (x - 1.) / (x + 1.);
+   z2 = z * z;
+   return( z * (2. + z2 * (2. / 3 + z2 * 0.4)) + (double)two_power * ln_2);
 }
 
 static void splash_screen( void)
