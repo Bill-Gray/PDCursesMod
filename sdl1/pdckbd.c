2968f3198d4ece0c28ef14860be9c5a1ead7df41
Bill-Gray <pluto@projectpluto.com>
2022-07-07 00:07:13 -0400

    Mark Hessling pointed out problems with SDL1 on OS/2 : Ctrl-(letter) and Alt-(letter) combinations are not returned.  Unfortunately,  this fix/workaround does assume a QWERTY layout.  Won't work well on (for example) Dvorak and non-US layouts.

--- sdl1/pdckbd.c
+++ sdl1/pdckbd.c
@@ -170,6 +170,23 @@ static int _process_key_event(void)
     if (!key)
     {
         key = event.key.keysym.unicode;
+#if defined(__OS2__)
+        if ( key == 0 && (event.key.keysym.mod & KMOD_CTRL || event.key.keysym.mod & KMOD_ALT) )
+        {
+            const int idx = event.key.keysym.scancode;
+            const int scancode_to_letter_length = 51;
+            static const char *scancode_to_letter =
+                   "                qwertyuiop    asdfghjkl     zxcvbnm";
+
+            if( idx >= 0 && idx < scancode_to_letter_length)
+                if( scancode_to_letter[idx] != ' ')
+                {
+                    key = scancode_to_letter[idx] - 'a' + 1;
+                    if ( event.key.keysym.mod & KMOD_ALT )
+                        key = key + 96;  /* for alt set to lower case alphabetic */
+                }
+        }
+#endif
 
         if (key > 0x7f)
             key = 0;
