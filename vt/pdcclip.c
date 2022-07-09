02d55b890e123e494f4cfe15fe9d235f87d24884
Bill-Gray <pluto@projectpluto.com>
2022-07-09 15:51:46 -0400

    Put Windows clipboard file into the 'common' directory,  to be shared by WinCon,  WinGUI,  and Windows builds of VT.  Maybe Windows SDL1 builds,  too,  eventually (should be straightforward to do).

--- vt/pdcclip.c
+++ vt/pdcclip.c
@@ -1,1 +1,8 @@
-#include "../common/pdcclip.c"
+#ifdef _WIN32
+   #include <windows.h>
+   #undef MOUSE_MOVED
+   #include <curspriv.h>
+   #include "../common/winclip.c"
+#else
+   #include "../common/pdcclip.c"
+#endif
