797f688dd449f70aa19b723b7cb48450167de9c6
Bill-Gray <pluto@projectpluto.com>
2022-07-09 23:23:37 -0400

    It's PDC_FORCE_UTF8 for make and nmake,  but PDC_UTF8 for CMake.

--- cmake/project_common.cmake
+++ cmake/project_common.cmake
@@ -85,7 +85,7 @@ if(PDC_BUILD_SHARED)
     endif()
 
     if(${PROJECT_NAME} STREQUAL "sdl2")
-        if(PDC_WIDE OR PDC_FORCE_UTF8)
+        if(PDC_WIDE OR PDC_UTF8)
             target_link_libraries(${PDCURSE_PROJ} ${EXTRA_LIBS}
                 ${SDL2_LIBRARIES} ${SDL2_TTF_LIBRARY} ${FT2_LIBRARY} ${ZLIB_LIBRARY}
                 ${SDL2_DEP_LIBRARIES})
