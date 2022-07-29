3cc99c12c65df174abb0ca5f785522bbd6860cad
Bill-Gray <pluto@projectpluto.com>
2022-07-29 18:03:59 -0400

    Time for a version update.  I expect there will be another one quite soon,  to include clangen's single-threaded WinGUI changes,  but thought it'd be good to have a 'changes to date' (there have been many) version followed by a 'single-threaded WinGUI' version.

--- curses.h
+++ curses.h
@@ -39,10 +39,10 @@ Defined by this header:
          /* the 'endwin_*' #defines below should be updated.             */
 #define PDC_VER_MAJOR    4
 #define PDC_VER_MINOR    3
-#define PDC_VER_CHANGE   3
+#define PDC_VER_CHANGE   4
 #define PDC_VER_YEAR   2022
 #define PDC_VER_MONTH    07
-#define PDC_VER_DAY      24
+#define PDC_VER_DAY      29
 
 #define PDC_STRINGIZE( x) #x
 #define PDC_stringize( x) PDC_STRINGIZE( x)
