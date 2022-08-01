6f5e8fb85eac27d9590c92b22748a0c3b8b8bda6
Bill-Gray <pluto@projectpluto.com>
2022-08-01 15:30:43 -0400

    WinGUI modified to make PDC_check_key() much,  much faster

--- wingui/pdckbd.c
+++ wingui/pdckbd.c
@@ -17,14 +17,28 @@ void PDC_set_keyboard_binary(bool on)
 extern int PDC_key_queue_low, PDC_key_queue_high;
 extern int PDC_key_queue[KEY_QUEUE_SIZE];
 
-/* PDCurses message/event callback */
-/* Calling PDC_napms for one millisecond ensures that the message loop */
-/* is called and messages in general,  and keyboard events in particular, */
-/* get processed.   */
+/* The PeekMessage( ) check for new keyhits is a little sluggish;
+it runs at about 70000/s on my machine.  The current logic is therefore :
+if a millisecond has not elapsed (i.e.,  curr_ms == last_ms),  there is
+probably no point in checking for messages.  With that modification,
+the relatively expensive PeekMessage( ) call is only done once every
+millisecond,  and PDC_check_key can take (on average) under half a
+microsecond.         */
 
 bool PDC_check_key(void)
 {
-    PDC_napms( 1);
+    MSG msg;
+    extern HWND PDC_hWnd;
+    DWORD curr_ms = GetTickCount( );
+    static DWORD last_ms;
+
+    if( curr_ms != last_ms)
+        while( PeekMessage(&msg, PDC_hWnd, WM_KEYFIRST, WM_KEYLAST, PM_REMOVE) )
+        {
+           TranslateMessage(&msg);
+           DispatchMessage(&msg);
+        }
+    last_ms = curr_ms;
     if( PDC_key_queue_low != PDC_key_queue_high)
         return TRUE;
     return FALSE;
