44416257d03748940c574be34aa1e94026cebbda
Bill-Gray <pluto@projectpluto.com>
2023-01-06 21:25:09 -0500

    VT : the window sizing handler ought to be set to use the terminal file handle.  That may be STDOUT_FILENO (if stdout hasn't been redirected to a file) or STDERR_FILENO (if stdout got redirected).  See preceding commit a6618d94d73efd7cc3ec5293.

--- vt/pdcscrn.c
+++ vt/pdcscrn.c
@@ -207,12 +207,15 @@ void PDC_scr_free( void)
 }
 
 #ifdef USE_TERMIOS
+
+int PDC_get_terminal_fd( void);        /* pdckbd.c */
+
 static void sigwinchHandler( int sig)
 {
    struct winsize ws;
 
    INTENTIONALLY_UNUSED_PARAMETER( sig);
-   if( -1 != ioctl( STDOUT_FILENO, TIOCGWINSZ, &ws))
+   if( -1 != ioctl( PDC_get_terminal_fd( ), TIOCGWINSZ, &ws))
       if( PDC_rows != ws.ws_row || PDC_cols != ws.ws_col)
          {
          PDC_rows = ws.ws_row;
