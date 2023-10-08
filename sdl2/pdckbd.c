88c51fbd3f1ecd6737f6389de0091d9dc1d0bdb2
Lyle-Tafoya <lyle.tafoya@gmail.com>
2023-10-08 04:31:52 -0700

    Eliminated numpad key repeat issue for sdl2 and gl on Windows. Fixes #304

--- sdl2/pdckbd.c
+++ sdl2/pdckbd.c
@@ -292,10 +292,8 @@ static int _process_key_event(void)
                 (key_table[i].numkeypad && (event.key.keysym.mod & KMOD_NUM)))
             {
                 key = key_table[i].shifted;
-#ifdef __linux
                 if( (key >= '0' && key <= '9') || strchr( ".+-*/", key))
                     key = -1;
-#endif
             }
             else if (event.key.keysym.mod & KMOD_CTRL)
             {
