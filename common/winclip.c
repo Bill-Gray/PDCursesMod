a071130c95cc498dc6182898bdd20fdff6bf9c20
Mark Hessling <github@rexx.org>
2024-02-10 01:25:06 +1000

    Clear clipboard requires clipboard to be opened (#316)
    Co-authored-by: Mark Hessling <mark@rexx.org>

--- common/winclip.c
+++ common/winclip.c
@@ -140,7 +140,12 @@ int PDC_clearclipboard(void)
 {
     PDC_LOG(("PDC_clearclipboard() - called\n"));
 
+    if (!OpenClipboard(NULL))
+        return PDC_CLIP_ACCESS_ERROR;
+
     EmptyClipboard();
+
+    CloseClipboard();
 
     return PDC_CLIP_SUCCESS;
 }
