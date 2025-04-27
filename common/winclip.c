5e94dfa7f95f066ee9350f4816c48e3fb8d77350
Bill-Gray <pluto@projectpluto.com>
2025-04-27 10:26:28 -0400

    WinCon/WinGUI PDC_clearclipboard() function now returns PDC_CLIP_SUCCESS only if the EmptyClipboard() call succeeds,  aligning with wmcbrine/PDCurses@26f091f0b60fb1e and wmcbrine/PDCurses@da691a62ebd1d69a23 .  Also some minor reformatting of associated documentation,  again to bring it into alignment with William McBrine's version.

--- common/winclip.c
+++ common/winclip.c
@@ -21,19 +21,18 @@ clipboard
    memory returned, via PDC_freeclipboard(). The length of the clipboard
    contents is returned in the length argument.
 
-   PDC_setclipboard copies the supplied text into the system's
+   PDC_setclipboard() copies the supplied text into the system's
    clipboard, emptying the clipboard prior to the copy.
 
    PDC_clearclipboard() clears the internal clipboard.
 
 ### Return Values
 
-   indicator of success/failure of call.
-   PDC_CLIP_SUCCESS        the call was successful
-   PDC_CLIP_MEMORY_ERROR   unable to allocate sufficient memory for
-                           the clipboard contents
-   PDC_CLIP_EMPTY          the clipboard contains no text
-   PDC_CLIP_ACCESS_ERROR   no clipboard support
+    PDC_CLIP_SUCCESS        the call was successful
+    PDC_CLIP_MEMORY_ERROR   unable to allocate sufficient memory for
+                            the clipboard contents
+    PDC_CLIP_EMPTY          the clipboard contains no text
+    PDC_CLIP_ACCESS_ERROR   no clipboard support
 
 ### Portability
                              X/Open  ncurses  NetBSD
@@ -138,14 +137,15 @@ int PDC_freeclipboard(char *contents)
 
 int PDC_clearclipboard(void)
 {
+    int rval = PDC_CLIP_ACCESS_ERROR;
+
     PDC_LOG(("PDC_clearclipboard() - called\n"));
 
-    if (!OpenClipboard(NULL))
-        return PDC_CLIP_ACCESS_ERROR;
-
-    EmptyClipboard();
-
-    CloseClipboard();
-
-    return PDC_CLIP_SUCCESS;
+    if (OpenClipboard(NULL))
+    {
+        if( EmptyClipboard())
+            rval = PDC_CLIP_SUCCESS;
+        CloseClipboard();
+    }
+    return rval;
 }
