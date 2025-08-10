5bd5a1d5355137cb7ca842018d1c60bfbb312d78
Bill-Gray <pluto@projectpluto.com>
2025-08-10 18:52:19 -0400

    Older compilers may lack MOUSE_WHEELED and/or MOUSE_HWHEELED

--- wincon/pdckbd.c
+++ wincon/pdckbd.c
@@ -617,13 +617,17 @@ static void _process_mouse_event(void)
 
     /* Handle scroll wheel */
 
+#ifdef HOUSE_WHEELED
     if (MEV.dwEventFlags == MOUSE_WHEELED)
         event = (MEV.dwButtonState & 0xFF000000) ?
                PDC_MOUSE_WHEEL_DOWN : PDC_MOUSE_WHEEL_UP;
+#endif
 
+#ifdef HOUSE_HWHEELED
     if (MEV.dwEventFlags == MOUSE_HWHEELED)
         event = (MEV.dwButtonState & 0xFF000000) ?
                PDC_MOUSE_WHEEL_RIGHT : PDC_MOUSE_WHEEL_LEFT;
+#endif
 
     if (MEV.dwEventFlags == 1)
         event = BUTTON_MOVED;
