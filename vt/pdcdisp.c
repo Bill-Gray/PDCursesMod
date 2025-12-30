Bill-Gray/PDCursesMod

commit 82e7a93542c3393c0a89fc46f74deb422bdb8bda

VT/pdcdisp.c modified to compile in non-wide builds (where MAX_UNICODE is not #defined).  Removed a no-longer-needed check for dummy characters when drawing text in PDC_transform_line().

vt/pdcdisp.c
@@ -285,8 +285,10 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
        chtype changes = *srcp ^ prev_ch;
        size_t bytes_out = 0;
 
+#ifdef PDC_WIDE
        assert( ch != MAX_UNICODE);
        assert( len == 1 || ch < MAX_UNICODE);
+#endif
        if( _is_altcharset( *srcp))
           ch = (int)acs_map[ch & 0x7f];
        if( ch < (int)' ' || (ch >= 0x80 && ch <= 0x9f))
@@ -347,9 +349,13 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
 #else
            bytes_out = PDC_wc_to_utf8( obuff, (wchar_t)ch);
 #endif
-           while( count < len && !((srcp[0] ^ srcp[count]) & ~A_CHARTEXT)
-                        && (ch = (srcp[count] & A_CHARTEXT)) < (int)MAX_UNICODE)
+           while( count < len && !((srcp[0] ^ srcp[count]) & ~A_CHARTEXT))
            {
+               ch = srcp[count] & A_CHARTEXT;
+#ifdef PDC_WIDE
+               assert( ch < MAX_UNICODE);
+#endif
+
                if( _is_altcharset( srcp[count]))
                   ch = (int)acs_map[ch & 0x7f];
 #ifdef DOS

commit e4b82a356c8924cb51a3226afc9d05a70908c7c0

Display of combining characters in the VT port was broken in commit 82e7a93542c3393

vt/pdcdisp.c
@@ -349,13 +349,15 @@ void PDC_transform_line(int lineno, int x, int len, const chtype *srcp)
 #else
            bytes_out = PDC_wc_to_utf8( obuff, (wchar_t)ch);
 #endif
+#ifdef PDC_WIDE
+           while( count < len && !((srcp[0] ^ srcp[count]) & ~A_CHARTEXT)
+                        && (ch = (srcp[count] & A_CHARTEXT)) < (int)MAX_UNICODE)
+           {
+#else
            while( count < len && !((srcp[0] ^ srcp[count]) & ~A_CHARTEXT))
            {
                ch = srcp[count] & A_CHARTEXT;
-#ifdef PDC_WIDE
-               assert( ch < MAX_UNICODE);
 #endif
-
                if( _is_altcharset( srcp[count]))
                   ch = (int)acs_map[ch & 0x7f];
 #ifdef DOS