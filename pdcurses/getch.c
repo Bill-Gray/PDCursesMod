8589665b7a617dcaed3be53b98fbb0036dcc2feb
Bill-Gray <pluto@projectpluto.com>
2026-03-28 13:58:34 -0400

    PDC_millisecs( ) should (now does) use CLOCK_MONOTONIC,  not CLOCK_REALTIME.
    PDC_millisecs( ) is always used in a relative mode;  it is time
    elapsed relative to an unspecified epoch.  CLOCK_REALTIME gets you
    'wall clock' time,  which is subject to corrections that may actually
    cause it to go backward.  All we really want is something that will
    tell us if (for example) half a second has elapsed since the cursor
    and blinking text were redrawn,  or if SP->mouse_wait milliseconds
    have elapsed after a mouse button press.  So for our purposes,
    CLOCK_MONOTONIC is the clock to use.
    
    Note that this is an almost meaningless change in practice.  But it
    does a better job of reflecting the purpose of PDC_millisecs( ),  and
    would matter somewhat if the clock got adjusted by a significant
    amount.

--- pdcurses/getch.c
+++ pdcurses/getch.c
@@ -469,7 +469,7 @@ long PDC_millisecs( void)
 {
     struct timespec t;
 
-    clock_gettime( CLOCK_REALTIME, &t);
+    clock_gettime( CLOCK_MONOTONIC, &t);
     return( t.tv_sec * 1000 + t.tv_nsec / 1000000);
 }
 
