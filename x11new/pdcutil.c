73afbbe3111007be16b012cbe107af4f1540ad4b
Bill-Gray <pluto@projectpluto.com>
2026-04-08 14:35:22 -0400

    x11new  now uses the 'common' beep routines (see commit b451847928d788).

--- x11new/pdcutil.c
+++ x11new/pdcutil.c
@@ -2,46 +2,33 @@
 #include <stdlib.h>
 #include "curspriv.h"
 #include "pdcx11.h"
+#include "../common/beep.c"
 
 void PDC_check_for_blinking( void);
 
-static void _raw_beep( void)
+void PDC_napms( const int ms)     /* 'ms' = milli,  _not_ microseconds! */
 {
-   if( !fork( ))        /* we're the child;  beep at 300 Hz for 0.1 second */
-      {
-      execlp( "play", "play", "-q", "-V0", "-n", "synth", "0.1", "sin", "300",
-                      (char *)NULL);
-                      /* if we get here,  play/SoX must not be installed */
-      flash( );       /* so we'll just flash the screen instead */
-      exit( 0);
-      }
-}
+    long curr_ms = PDC_millisecs( );
+    const long end_t = curr_ms + ms;
+    int remains;
 
-void PDC_beep(void)
-{
-    if( !SP->n_beeps_queued)
-       {
-       const long beep_interval = 400;
+    PDC_LOG(("PDC_napms() - called: ms=%d\n", ms));
 
-       _raw_beep( );
-       SP->t_next_beep = PDC_millisecs( ) + beep_interval;
-       }
-    SP->n_beeps_queued++;
-}
-
-void PDC_napms(int ms)
-{
-    PDC_check_for_blinking( );
-    while( ms > 0)
+    do
     {
-        const int ms_to_nap = (ms > 50 ? 50 : ms);
-
-        usleep( 1000 * ms_to_nap);
-        ms -= ms_to_nap;
+        remains = (int)( end_t - curr_ms);
         PDC_check_for_blinking( );
-    }
-}
+        if( remains > 0)
+        {
+            const int max_sleep_ms = 50;      /* check msgs 20 times/second */
 
+            if( remains > max_sleep_ms)
+                remains = max_sleep_ms;
+            usleep( remains * 1000);
+            curr_ms = PDC_millisecs( );
+        }
+    } while( remains > 0);
+}
 
 const char *PDC_sysname(void)
 {
