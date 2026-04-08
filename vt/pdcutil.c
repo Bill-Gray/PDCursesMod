3de318257af3dcb9b0633626a9835fc96dc419fe
Bill-Gray <pluto@projectpluto.com>
2026-04-08 14:04:38 -0400

    VT port now beeps,  using the logic described in commit b451847928d78.

--- vt/pdcutil.c
+++ vt/pdcutil.c
@@ -2,8 +2,9 @@
    #define WIN32_LEAN_AND_MEAN
    #include <windows.h>
    #undef MOUSE_MOVED
-#else
+#elif !defined( DOS)
    #include <unistd.h>
+   #include <stdlib.h>
 #endif
 #include "curspriv.h"
 
@@ -13,34 +14,56 @@
    #include "../common/dosutil.c"
 #else
 
-void PDC_beep(void)
-{
-}
+#include "../common/beep.c"
+
+#ifdef _WIN32
+void PDC_check_for_resize( void);            /* pdcscrn.c */
+#endif
+
+#ifdef LINUX_FRAMEBUFFER_PORT
+   void PDC_check_for_blinking( void);       /* ../common/blink.c */
+#endif
 
 void PDC_napms(int ms)
 {
-#ifdef _WIN32
-    Sleep(ms);
-#else
-#ifndef DOS
-#ifdef LINUX_FRAMEBUFFER_PORT
-    PDC_check_for_blinking( );
-    while( ms > 0)
-    {
-        const int ms_to_nap = (ms > 50 ? 50 : ms);
+    long t = PDC_millisecs( );
+    const long end_t = t + ms;
+    int remains;
 
-        usleep( 1000 * ms_to_nap);
-        ms -= ms_to_nap;
-        PDC_check_for_blinking( );
-    }
+    do
+    {
+        remains = (int)( end_t - t);
+
+        if( SP->n_beeps_queued && (int)( t - SP->t_next_beep) > 0)
+        {
+            SP->n_beeps_queued--;
+            SP->t_next_beep = t + beep_interval;
+            if( SP->n_beeps_queued > 0)
+               _raw_beep( );
+        }
+        if( remains > 50)        /* break into 50 ms bits */
+            remains = 50;
+#ifdef LINUX_FRAMEBUFFER_PORT
+             PDC_check_for_blinking( );
+#endif
+#ifdef _WIN32
+        PDC_check_for_resize( );
+        if( remains > 0)
+        {
+            Sleep( remains);
+            t = PDC_millisecs( );
+        }
 #else
-    usleep(1000 * ms);
+        if( remains > 0)
+        {
+            usleep( 1000 * remains);
+            t = PDC_millisecs( );
+        }
 #endif
-#endif
-#endif
+    }
+    while( remains > 0);
 }
 #endif               /* non-DOS case */
-
 
 const char *PDC_sysname(void)
 {
