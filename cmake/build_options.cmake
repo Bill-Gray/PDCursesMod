8bd7ad6b7ac2436eec101f6b265bd461bec1c74e
frostbane <frostbane@programmer.net>
2026-04-10 22:49:44 +0900

    Add x11new to the main CMakeLists.txt

--- cmake/build_options.cmake
+++ cmake/build_options.cmake
@@ -11,6 +11,7 @@ option(PDC_DOSVT_BUILD "Build VT Project" OFF)
 option(PDC_SDL2_BUILD "Build SDL2 Project" ON)
 option(PDC_SDL2_DEPS_BUILD "Build SDL2 and dependencies" ON)
 option(PDC_GL_BUILD "Build OpenGL Project" ON)
+option(PDC_X11_BUILD "Build X11 Project" OFF)
 
 message(STATUS "PDC_BUILD_SHARED ....... ${PDC_BUILD_SHARED}")
 message(STATUS "PDC_UTF8 ............... ${PDC_UTF8}")
@@ -24,6 +25,7 @@ message(STATUS "PDC_DOSVT_BUILD ........ ${PDC_DOSVT_BUILD}")
 message(STATUS "PDC_SDL2_BUILD ......... ${PDC_SDL2_BUILD}")
 message(STATUS "PDC_SDL2_DEPS_BUILD .... ${PDC_SDL2_DEPS_BUILD}")
 message(STATUS "PDC_GL_BUILD ........... ${PDC_GL_BUILD}")
+message(STATUS "PDC_X11_BUILD .......... ${PDC_X11_BUILD}")
 
 # normalize a windows path
 file(TO_CMAKE_PATH "${CMAKE_INSTALL_PREFIX}" CMAKE_INSTALL_PREFIX)
